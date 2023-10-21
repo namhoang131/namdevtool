b2859e55126fa40ceaa181aafccfdb9caf6d1d69
NamTool131 <143366877+namhoang131@users.noreply.github.com>
2023-10-21 17:15:31 +0700

    tdsfbv1.py

--- tdsfbv1.py
+++ tdsfbv1.py
@@ -53,7 +53,7 @@ banners=f"""
             \033[1;34m Nhóm Zalo : \033[1;37mhttps://zalo.me/g/kfmgqm225
             \033[1;34m Facebook   : \033[1;37mhttps://facebook.com/nam.nhn131 
             \033[1;36m╰─────────────────────────────────────────────────────⋟─╯ 
-            \033[1;31m              TOOL TRAO ĐỔI SUB FACEBOOK
+            \033[1;31m              TOOL TRAO ĐỔI SUB FACEBOOK V1
             \033[1;36m╰─────────────────────────────────────────────────────⋟─╯"""     
 
 def lehoangphuc(so):
